50ef2a5452c3bb031583b15b4b03b04e8aa9c34d
Samuel Gaviria <samu0825369@gmail.com>
2025-06-17 09:51:40 -0500

    Revert "Merge branch 'master' of https://github.com/liril-co/trivium"
    This reverts commit 34d4b32330eddc001854f0a63218ae24510aa176, reversing
    changes made to ac4358a0ee9c717e6503bdeba5e4bfd114ae7ca7.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8" />
@@ -131,6 +132,75 @@
             </section>
                     </template>
                     <template x-if="section == 'about'">
+                    <section id="products" class="relevant-content" x-data="productos">
+            <template x-if="!!products">
+                <template x-for="(product, index) in products" :key="product.id">
+                    <div x-data="producto" class="product-container" x-init="producto= product">
+                        <article class="product">
+                            <div class="slideshow-container"  x-ref="slideshow-container" @mousemove="appearControls()" :data-index="slideshowIndex">
+                                <figure class="slideshow">
+                                    <template x-for="(image, index) in product.imagenes" :key="index">
+                                        <img :src="`/storage/${image}`" alt @click="openImageModal(`/storage/${image}`, index)" style="cursor: pointer;">                                    </template>
+                                </figure>
+                                <div class="prev-image"  x-ref="prev-image" @click="prevSlideshowImage()">
+                                    <i class="fa-solid fa-circle-chevron-left"></i>
+                                </div>
+                                <div class="next-image"  x-ref="next-image" @click="nextSlideshowImage()">
+                                    <i class="fa-solid fa-circle-chevron-right"></i>
+                                </div>
+                                <div class="controls visible"  x-ref="controls" >
+                                    <template x-for="(image, indexI) in product.imagenes" :key="indexI">
+                                        <img @click="updateSlideshow(indexI)" :class="indexI == 0 ? 'active' : ''"
+                                            :src="`/storage/${image}`" alt>
+                                    </template>
+                                </div>
+                                <div class="scroll-left-controls visible"   x-ref="scroll-left-controls" @mouseenter="scrollLeft"
+                                    @mouseleave="clearInterval(intervalScroll)">
+                                </div>
+                                <div class="scroll-right-controls visible"  x-ref="scroll-right-controls"  @mouseenter="scrollRight"
+                                    @mouseleave="clearInterval(intervalScroll)">
+                                </div>
+                            </div>
+                            <div class="right">
+                                <h1 x-text="product.nombre"></h1>
+                                <p class="description" x-text="product.descripcion"></p>
+
+                                </p>
+                                <div class="info">
+                                    <div class="left"  x-ref="left" ><span class="price" x-text="`$ ${product.precio}`"></span></div>
+                                    <div class="right"  x-ref="right" >
+                                        <input type="number" x-ref="`quantity_product_${product.id}`" name="quantity" class="quantity"
+                                            value="1" autocomplete="off">
+                                        <div class="add-to-cart" @click="addToCart(product, quantity)">
+                                            <i class="fa-solid fa-cart-shopping"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div x-show="showImageModal" class="product-modal" @click.self="closeImageModal">
+                                <button class="close-modal" @click.stop="closeImageModal()">
+                                    <i class="fa-solid fa-xmark"></i>
+                                </button>
+                                <button class="left-arrow" @click.stop="prevModalImage()" >
+                                    <i class="fa-solid fa-chevron-left"></i>
+                                </button>
+                                <img :src="modalImageUrl">
+                                <button class="right-arrow" @click.stop="nextModalImage()" >
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                </button>
+                            </div>
+                        </article>
+                    </div>
+                </template>
+            </template>
+            <template x-if="!products">
+                <div class="loading">
+                    <p>Cargando productos...</p>
+                </div>
+            </template>
+        </section>
+                </template>
+                <template x-if="section == 'about'">
 
                         <section id="about" x-ref="about" class="relevant-content" @scroll="handleInnerScroll($el)">
                             <div class="paragraph">
@@ -226,7 +296,144 @@
                         </section>
                     </template>
                     <template x-if="section == 'register'">
+                        <section id="about" x-ref="about" class="relevant-content" @scroll="handleInnerScroll($el)">
+                            <div class="paragraph">
+                                <img src="{{ asset('images/welcome/TRIVIUM-36.jpg') }}" alt>
+                                <p>
+                                    Así fue como nació Trivium, nuestro emprendimiento de cerveza
+                                    artesanal. Desde hace años, los tres compartimos una pasión
+                                    profunda por la buena cerveza. Nos encantaba explorar
+                                    diferentes
+                                    estilos y probar cervezas artesanales de todo el mundo,
+                                    maravillándonos con la variedad de sabores, aromas y la
+                                    calidad
+                                    que estas ofrecían, gracias a sus técnicas tradicionales y
+                                    cuidado
+                                    artesanal.
+                                    <br>
+                                    <br>
+                                    En 2022, decidimos que era momento de llevar nuestra pasión un
+                                    paso más allá. Nos inscribimos en un curso intensivo de
+                                    cerveza
+                                    artesanal, donde aprendimos desde los fundamentos básicos
+                                    hasta
+                                    técnicas avanzadas de elaboración. Durante semanas, nos
+                                    sumergimos en el proceso detallado de malteado, maceración,
+                                    hervido, fermentación y embotellado. Aprendimos sobre la
+                                    importancia de los ingredientes de calidad, las levaduras
+                                    adecuadas
+                                    para cada estilo y cómo controlar parámetros clave como la
+                                    temperatura y el pH para obtener resultados consistentes y
+                                    deliciosos.
+                                </p>
+                            </div>
+                            <div class="paragraph">
+                                <p>
+                                    Así fue como nació XXXX, nuestro emprendimiento de cerveza
+                                    artesanal. Desde hace años, los tres compartimos una pasión
+                                    profunda por la buena cerveza. Nos encantaba explorar
+                                    diferentes
+                                    estilos y probar cervezas artesanales de todo el mundo,
+                                    maravillándonos con la variedad de sabores, aromas y la
+                                    calidad
+                                    que estas ofrecían, gracias a sus técnicas tradicionales y
+                                    cuidado
+                                    artesanal.
+                                    <br>
+                                    <br>
+                                    En 2022, decidimos que era momento de llevar nuestra pasión un
+                                    paso más allá. Nos inscribimos en un curso intensivo de
+                                    cerveza
+                                    artesanal, donde aprendimos desde los fundamentos básicos
+                                    hasta
+                                    técnicas avanzadas de elaboración. Durante semanas, nos
+                                    sumergimos en el proceso detallado de malteado, maceración,
+                                    hervido, fermentación y embotellado. Aprendimos sobre la
+                                    importancia de los ingredientes de calidad, las levaduras
+                                    adecuadas
+                                    para cada estilo y cómo controlar parámetros clave como la
+                                    temperatura y el pH para obtener resultados consistentes y
+                                    deliciosos.
+                                </p>
+                                <img src="{{ asset('images/welcome/TRIVIUM-36.jpg') }}" alt>
+                            </div>
+                            <div class="paragraph vertical">
+                                <img src="{{ asset('images/welcome/TRIVIUM-38.jpg') }}" alt>
+                                <p>
+                                    Así fue como nació XXXX, nuestro emprendimiento de cerveza
+                                    artesanal. Desde hace años, los tres compartimos una pasión
+                                    profunda por la buena cerveza. Nos encantaba explorar
+                                    diferentes
+                                    estilos y probar cervezas artesanales de todo el mundo,
+                                    maravillándonos con la variedad de sabores, aromas y la
+                                    calidad
+                                    que estas ofrecían, gracias a sus técnicas tradicionales y
+                                    cuidado
+                                    artesanal.
+                                    <br>
+                                    <br>
+                                    En 2022, decidimos que era momento de llevar nuestra pasión un
+                                    paso más allá. Nos inscribimos en un curso intensivo de
+                                    cerveza
+                                    artesanal, donde aprendimos desde los fundamentos básicos
+                                    hasta
+                                    técnicas avanzadas de elaboración. Durante semanas, nos
+                                    sumergimos en el proceso detallado de malteado, maceración,
+                                    hervido, fermentación y embotellado. Aprendimos sobre la
+                                    importancia de los ingredientes de calidad, las levaduras
+                                    adecuadas
+                                    para cada estilo y cómo controlar parámetros clave como la
+                                    temperatura y el pH para obtener resultados consistentes y
+                                    deliciosos.
+                                </p>
+                            </div>
+                        </section>
+                    </template>
+                    <template x-if="section == 'register'">
 
+                        <section id="register" x-ref="register" class="relevant-content" @scroll="handleInnerScroll($el)">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <label for="username">
+                                    <i class="fa-solid fa-user"></i>
+                                    <input type="text" name="username" placeholder="Nombre de usuario">
+                                </label>
+                                <label for="name">
+                                    <i class="fa-solid fa-id-card"></i>
+                                    <input type="text" name="name" placeholder="Nombres y apellidos">
+                                </label>
+                                <label for="cellphone">
+                                    <i class="fa-solid fa-mobile-screen-button"></i>
+                                    <input type="text" name="cellphone" placeholder="Número de celular">
+                                </label>
+                                <label for="email">
+                                    <i class="fa-solid fa-envelope"></i>
+                                    <input type="email" name="email" placeholder="Correo electrónico">
+                                </label>
+                                <label for="email_confirmation">
+                                    <i class="fa-solid fa-envelope"></i>
+                                    <input type="email" name="email_confirmation"
+                                        placeholder="Confirma el correo electrónico">
+                                </label>
+                                <label for="password">
+                                    <i class="fa-solid fa-key"></i>
+                                    <input type="password" name="password" placeholder="Crea una contraseña">
+                                </label>
+                                <label for="password_confirmation">
+                                    <i class="fa-solid fa-key"></i>
+                                    <input type="password" name="password_confirmation"
+                                        placeholder="Confirma la contraseña">
+                                </label>
+                                <button type="submit" class="fa-solid fa-user-plus"></button>
+                            </form>
+                            <figure>
+                                <img src="{{ asset('images/welcome/TRIVIUM-Texto-Sin Fondo.png') }}" alt>
+                            </figure>
+                        </section>
+                    </template>
+                </article>
+            </section>
+        </aside>
                         <section id="register" x-ref="register" class="relevant-content" @scroll="handleInnerScroll($el)">
                             <form method="POST" action="{{ route('register') }}">
                                 @csrf
